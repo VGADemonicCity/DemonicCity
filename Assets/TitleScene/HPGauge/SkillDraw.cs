109fae165af879d095c81a78f27b3bcb2c1c169e
PS18A05\laver <laver9692@gmail.com>
2018-11-10 00:24:00 +0900

    SkillGauge修正

--- Assets/TitleScene/HPGauge/SkillDraw.cs
+++ Assets/TitleScene/HPGauge/SkillDraw.cs
@@ -7,11 +7,11 @@ namespace DemonicCity.BattleScene
 {
     public class SkillDraw : MonoBehaviour
     {
-        enum SkillStatus { None=0,Half=1,Full=2}
+        enum SkillStatus { None = 0, Half = 1, Full = 2 }
         public Image[] flames = new Image[3];
         int maxSkill = 10;
         int currentSkill = 0;
-        double ratio = 0;
+        double ratio;
 
         public void Initialize(int max)
         {
@@ -20,32 +20,38 @@ namespace DemonicCity.BattleScene
             Reflect(currentSkill);
         }
 
-        public void Reflect(int skill)
+        public void Reflect(float skill)
         {
             ratio = skill / maxSkill;
-            if (ratio>0)
+            if (ratio > 0)
             {
                 flames[(int)SkillStatus.Half].color = new Color(1, 1, 1, (float)ratio * 2);
             }
-            if (ratio>0.5)
+            if (ratio > 0.5)
             {
-                flames[(int)SkillStatus.Full].color = new Color(1, 1, 1, (float)(ratio-0.5) * 2);
-
+                flames[(int)SkillStatus.Full].color = new Color(1, 1, 1, (float)(ratio - 0.5) * 2);
                 flames[(int)SkillStatus.None].color = Color.clear;
             }
-            if (ratio>=1)
+            if (ratio >= 1)
             {
                 flames[(int)SkillStatus.Full].color = Color.white;
-
                 flames[(int)SkillStatus.Half].color = Color.clear;
             }
         }
 
+        public void SkillReset()
+        {
+            currentSkill = 0;
+            Reflect(0);
+        }
 
         public void Skill1Up()
         {
-            currentSkill += 1;
-            Reflect(currentSkill);
+            if (maxSkill > currentSkill)
+            {
+                currentSkill += 1;
+                Reflect(currentSkill);
+            }
         }
 
 
