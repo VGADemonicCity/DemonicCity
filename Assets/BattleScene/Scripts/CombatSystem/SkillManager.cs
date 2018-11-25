5cbacb2ef510e044b61c25255c7fa137f08f3ba2
PS18A05\laver <laver9692@gmail.com>
2018-11-26 04:50:29 +0900

    Merge branch 'Develop' of https://github.com/VGADemonicCity/DemonicCity into Develop

--- Assets/BattleScene/Scripts/CombatSystem/SkillManager.cs
+++ Assets/BattleScene/Scripts/CombatSystem/SkillManager.cs
@@ -13,34 +13,6 @@ namespace DemonicCity.BattleScene
         /// <summary>SkillProcessor</summary>
         public SkillJudger m_skillJudger = new SkillJudger();
 
-        /// <summary>シャッフルスキル発動条件値</summary>
-        [SerializeField] int m_shuffleSkillConditions = 30;
-       
-        /// <summary>PanelCounterの参照</summary>
-        PanelCounter m_panelCounter;
-
-
-        /// <summary>
-        /// Awake this instance.
-        /// </summary>
-        void Awake()
-        {
-            m_panelCounter = GetComponent<PanelCounter>(); // PanelCounterの参照取得
-        }
-
-        /// <summary>
-        /// シャッフルスキルを発動したら、現在画面に表示されている3*3のパネルを全て非表示の状態に戻して、その3*3のパネル内でまたシャッフルさせる
-        /// </summary>
-        void ShufflePanels()
-        {
-            var shuffleCounter = m_panelCounter.m_CounterForShuffleSkill; // シャッフルスキル用のカウント数取得
-            if (shuffleCounter >= m_shuffleSkillConditions) // 設定した回数以上パネルが引かれていたら
-            {
-                var panelManager = PanelManager.Instance; // PanelManagerの参照
-                panelManager.InitPanels(); // パネルを初期化する
-                m_panelCounter.ResetShuffleSkillCounter(); // カウンターをリセット
-            }
-        }
 
         /// <summary>
         /// 条件に沿ってパッシブスキルを発動させるかどうかを判断して、条件を満たすスキルの効果を反映させるイベント
