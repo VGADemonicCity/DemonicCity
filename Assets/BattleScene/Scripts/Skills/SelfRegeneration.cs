c194941cd682230e4b19824bad67a2553fb720bf
Zukky_13 <www.kase@gmail.com>
2018-12-06 03:28:55 +0900

    各パッシブスキルの実装作業中

--- Assets/BattleScene/Scripts/Skills/SelfRegeneration.cs
+++ Assets/BattleScene/Scripts/Skills/SelfRegeneration.cs
@@ -21,22 +21,14 @@ namespace DemonicCity.BattleScene.Skill
         }
 
         /// <summary>
-        /// Start this instance.
-        /// </summary>
-        protected override void Start()
-        {
-            base.Start();
-        }
-
-        /// <summary>
-        /// 魔拳
-        /// 街破壊数1以上で発動.
-        /// 街破壊数 * 攻撃力の1% を加算して攻撃
+        /// スキル発動
         /// </summary>
         protected override void SkillActivate()
         {
             Debug.Log("Activated the 魔拳");
             var count = m_panelCounter.GetCityDestructionCount(); // 街破壊数
+            //m_magia.Stats.
+            //m_magia.Stats.m_attack += (int)(count * m_magia.Stats.m_attack * m_increase); // 攻撃力の任意の%分加算
 
         }
     }
