4a42c6b2b98757de095b7dfbbd622086643a9723
RyoOkazaki <ryolavieg@hotmail.co.jp>
2018-12-07 20:21:56 +0900

    強化画面のUI配置

---
+++ Assets/StrengthenScene/Scripts/SceneLoadManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoadManager : MonoBehaviour
+{
+    private Animator animator;
+    
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void NextAnimation()
+    {
+        animator.SetBool("isPlay", true);
+    }
+}
