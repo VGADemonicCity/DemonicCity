bfffcb77da2d37504bef0d336c496a23b75d8532
RyoOkazaki <ryolavieg@hotmail.co.jp>
2018-12-08 14:56:01 +0900

    仕様書に基づいたレイアウトの作成

---
+++ Assets/StrengthenScene/Scripts/SceneManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DemonicCity.StrengthenScene
+{
+    public class SceneManager : MonoBehaviour
+    {
+        public GameObject ShikiryoWindow;
+        public GameObject SkillWindow;
+        Animation animation;
+
+        void Start()
+        {
+            ShikiryoWindow.SetActive(false);
+            SkillWindow.SetActive(false);
+
+            animation = ShikiryoWindow.GetComponent<Animation>();
+            animation = SkillWindow.GetComponent<Animation>();
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void OpenShikiryoWindow()
+        {
+            animation.Play("OpenShikiryoWindow");
+            ShikiryoWindow.SetActive(true);
+        }
+        public void CloseShiryoWindow()
+        {
+            //animation.Play("CloseShiryoWindow");
+            ShikiryoWindow.SetActive(false);
+            
+        }
+        public void OpenSkillWindow()
+        {
+            animation.Play("OpenSkillWindow");
+            SkillWindow.SetActive(true);
+        }
+        public void CloseSkillWindow()
+        {
+            SkillWindow.SetActive(false);
+        }
+    }
+}
