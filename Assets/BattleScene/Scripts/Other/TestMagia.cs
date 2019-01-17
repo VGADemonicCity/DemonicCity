0d306de26d92d08da17d6eb92d60e9fd16c9a134
忰熊一輝 <www.kase@gmail.com>
2019-01-17 18:08:02 +0900

    LevelUpを他の形態とレベルアップ直前のレベル値に合わせた上昇値に対応させる為にクラスLevelingSystemとして切り出す。その作業中

---
+++ Assets/BattleScene/Scripts/Other/TestMagia.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DemonicCity
+{
+    public class TestMagia : Magia
+    {
+        // Use this for initialization
+        void Start()
+        {
+            LevelingSystem<TestMagia> magia;
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+    
+
+}
