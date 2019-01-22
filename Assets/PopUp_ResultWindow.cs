bf858de3cfcb83a6a7df7c5965bc322e0f8b8b2a
RyoOkazaki <ryolavieg@hotmail.co.jp>
2019-01-22 11:29:28 +0900

    リザルト画面のプレファブにアニメーション追加

---
+++ Assets/PopUp_ResultWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DemonicCity.ResultScene
+{
+    public class PopUp_ResultWindow : MonoBehaviour
+    {
+        [SerializeField] private GameObject resultWindow = null;
+
+        void Start()
+        {
+            Instantiate(resultWindow);
+        }
+    }
+}
