f61309ec2ed2066654934ce99449e61972b6c999
PS18A05\laver <laver9692@gmail.com>
2019-01-23 16:15:29 +0900

    Story関連のシーン遷移周りを修正

--- Assets/Scripts/Chapter.cs
+++ Assets/Scripts/Chapter.cs
@@ -18,12 +18,14 @@ namespace DemonicCity
         public string chapterTitle;
         /// <summary>その章に戦闘があるか</summary>
         public bool isBattle;
+        /// <summary>その章に会話があるか</summary>
+        public bool isStory;
         /// <summary>その章のソースフォルダのパス</summary>
         public string sourceFolderPath;
 
 
         /// <summary>ストーリーの進行度とタイトル、敵のIDを3Wave分登録する</summary>
-        public Chapter(string title, Progress.StoryProgress progress, EnemiesFactory.EnemiesId enemy1, EnemiesFactory.EnemiesId enemy2, EnemiesFactory.EnemiesId enemy3, string folderPath)
+        public Chapter(string title, Progress.StoryProgress progress, EnemiesFactory.EnemiesId enemy1, EnemiesFactory.EnemiesId enemy2, EnemiesFactory.EnemiesId enemy3, string folderPath,bool story=true)
         {
             chapterTitle = title;
             storyProgress = progress;
@@ -32,14 +34,16 @@ namespace DemonicCity
             enemiesIds.Add(enemy2);
             enemiesIds.Add(enemy3);
             isBattle = true;
+            isStory = story;
         }
         /// <summary>ストーリーの進行度とタイトルを登録する</summary>
-        public Chapter(string title, Progress.StoryProgress progress,string folderPath)
+        public Chapter(string title, Progress.StoryProgress progress,string folderPath,bool story=true)
         {
             chapterTitle = title;
             storyProgress = progress;
             sourceFolderPath = folderPath;
             isBattle = false;
+            isStory = story;
         }
     }
 }
