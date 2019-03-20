b9b48a3602e17e72cf3fef960c02c719f0a18ce6
RyoOkazaki <ryolavieg@hotmail.co.jp>
2019-03-20 16:51:48 +0900

    強化画面8割できたと思われる

--- Assets/StrengthenScene/Scripts/Strengthen_Part1.cs
+++ Assets/StrengthenScene/Scripts/Strengthen_Part1.cs
@@ -51,21 +51,8 @@ namespace DemonicCity.StrengthenScene
         /// <summary>現在の威厳値</summary>
         private int dignity;
 
-        private int defaultHp;
-        private int defaultAttack;
-        private int defaultDefence;
-        private int defaultDurebility;
-        private int defaultMuscularStrength;
-        private int defaultKnowledge;
-        private int defaultSence;
-        private int defaultCharm;
-        private int defaultDignity;
-
         /// <summary>割り振りポイント(魔力値)</summary>
         private int statusPoint;
-
-        /// <summary>割り振られたポイントの合計値</summary>
-        private int allocationPoint;
 
         /// <summary>割り振られた魅力値</summary>
         private int addCharm;
@@ -144,7 +131,7 @@ namespace DemonicCity.StrengthenScene
             skillExplanationList.Add("自分の攻撃力2分の1を敵に防御力を\n無視してダメージを与える");
             skillExplanationList.Add("街破壊数19以上で発動\n次の敵の攻撃を5%軽減");
             skillExplanationList.Add("街破壊数21以上で発動\n街破壊数×攻撃力の0.5%を加算して攻撃");
-            skillExplanationList.Add("街破壊数24以上で発動　街破壊数×0.5%攻撃力上昇");
+            skillExplanationList.Add("街破壊数24以上で発動\n街破壊数×0.5%攻撃力上昇");
             skillExplanationList.Add("街破壊数27以上で発動\n次の敵の攻撃を10%軽減");
             skillExplanationList.Add("破壊数30以上で発動\n破壊数×攻撃力の0.5%を加算して攻撃");
             skillExplanationList.Add("街破壊数32枚以上で発動\n街破壊数×最大HPの2%回復");
@@ -153,9 +140,7 @@ namespace DemonicCity.StrengthenScene
             skillExplanationList.Add("街破壊数36以上で発動\n次の敵の攻撃を無効化");
             skillExplanationList.Add("スキル発動枚数-10");
 
-            //ResetStatus();
-
-            var 
+            ResetStatus();
 
             touchGestureDetector.onGestureDetected.AddListener((gesture, touchInfo) =>
             {
@@ -166,7 +151,7 @@ namespace DemonicCity.StrengthenScene
 
                     if (button != null)
                     {
-                      
+
                         switch (button.name)
                         {
                             case "BackToHome":
@@ -260,32 +245,26 @@ namespace DemonicCity.StrengthenScene
                                 {
                                     confirmMessageWindow.SetActive(true);
                                     activePopUpWindow = true;
-                                    confirmMessageWindow.GetComponentInChildren<Text>().text = "変更したステータスを確定します";
                                 }
                                 break;
 
                             case "ResetButton":
                                 if (!activePopUpWindow)
                                 {
-                                    confirmMessageWindow.SetActive(true);
+                                    resetMessageWindow.SetActive(true);
                                     activePopUpWindow = true;
-                                    confirmMessageWindow.GetComponentInChildren<Text>().text = "変更したステータスを初期化します";
                                 }
                                 break;
 
                             case "YesConfirm":
                                 ConfirmStatus();
-
-                                if (activePopUpWindow)
-                                {
-                                    confirmMessageWindow.SetActive(false);
-                                    activePopUpWindow = false;
-                                }
-                                changedStatus = false;
                                 break;
 
                             case "YesReset":
                                 ResetStatus();
+
+                                confirmMessageWindow.SetActive(false);
+                                activePopUpWindow = false;
                                 break;
 
                             case "No":
@@ -304,19 +283,18 @@ namespace DemonicCity.StrengthenScene
 
         private void Update()
         {
-            if (changedStatus && allocationPoint > 0)
+            if (changedStatus)
             {
                 confirmResetButtons.SetActive(true);
             }
-            else if (!changedStatus && allocationPoint <= 0)
+            else if (!changedStatus)
             {
                 confirmResetButtons.SetActive(false);
             }
         }
 
         /// <summary>スキルの説明テキストを表示/非表示</summary>
-        /// <param name="index"></param>
-        public void SkillExplanationManager(int index)
+        private void SkillExplanationManager(int index)
         {
             if (setActiveSkillExplanation)
             {
@@ -326,15 +304,40 @@ namespace DemonicCity.StrengthenScene
             else
             {
                 skillExplanationText[index].SetActive(true);
+                ScrollToCore(skillListWindow.GetComponentInChildren<ScrollRect>(), skillExplanationText[index], 0);
+                Debug.Log(skillExplanationText[index].GetComponent<RectTransform>().rect.y);
                 setActiveSkillExplanation = true;
             }
+        }
+
+        ///<summary>ScrollRectのスクロール位置をGameObjectにあわせる</summary>
+        /// <param name="align">0:下、0.5:中央、1:上</param>
+        private float ScrollToCore(ScrollRect scrollRect, GameObject go, float align)
+        {
+            var targetRect = go.transform.GetComponent<RectTransform>();
+            var contentHeight = scrollRect.content.rect.height;
+            var viewportHeight = scrollRect.viewport.rect.height;
+            // スクロール不要
+            if (contentHeight < viewportHeight)
+            {
+                return 0f;
+            }
+            // ローカル座標が contentHeight の上辺を0として負の値で格納されてる
+            // これは現在のレイアウト特有なのかもしれないので、要確認
+            var targetPos = contentHeight + (targetRect.localPosition.y + targetRect.rect.y) + targetRect.rect.height * align;
+            var gap = viewportHeight * align; // 上端〜下端あわせのための調整量
+            var normalizedPos = (targetPos - gap) / (contentHeight - viewportHeight);
+
+            normalizedPos = Mathf.Clamp01(normalizedPos);
+            scrollRect.verticalNormalizedPosition = normalizedPos;
+            return normalizedPos;
         }
 
         /// <summary>魔力値を固有ステータスに割り振り、基礎ステータスに変換する</summary>
         /// <param name="uniqueStatus">固有ステータス</param>
         /// <param name="uniqueStatusText">固有ステータスのテキスト</param>
         /// <param name="addStatus">ステータスの増減判定</param>
-        public void ChangeUniqueStatus(ref int uniqueStatus, ref TextMeshProUGUI uniqueStatusText)
+        private void ChangeUniqueStatus(ref int uniqueStatus, ref TextMeshProUGUI uniqueStatusText)
         {
             uniqueStatus += AddStatusPoint(uniqueStatus);
             uniqueStatusText.text = "+" + uniqueStatus.ToString();
@@ -352,7 +355,7 @@ namespace DemonicCity.StrengthenScene
         }
 
         /// <summary>ステータスの変動値を初期化</summary>
-        public void ResetStatus()
+        private void ResetStatus()
         {
             for (int i = 0; i < skillExplanationText.Length; i++)
             {
@@ -362,35 +365,23 @@ namespace DemonicCity.StrengthenScene
 
             skillListWindow.SetActive(false);
             resetMessageWindow.SetActive(false);
-            confirmMessageWindow.SetActive(false);
-
+          
             activePopUpWindow = false;
 
-            changedStatus = false;
-
+           
             passiveSkill = magia.MyPassiveSkill;
 
-            var stats = magia.GetStats();
+            var status = magia.GetStats();
 
-            hp = stats.HitPoint;
-            attack = stats.Attack;
-            defense = stats.Defense;
-            charm = stats.Charm;
-            dignity = stats.Dignity;
-            muscularStrength = stats.MuscularStrength;
-            sense = stats.Sense;
-            durability = stats.Durability;
-            knowledge = stats.Knowledge;
-
-            defaultHp = hp;
-            defaultAttack = attack;
-            defaultDefence = defense;
-            defaultCharm = charm;
-            defaultDignity = dignity;
-            defaultMuscularStrength = muscularStrength;
-            defaultSence = sense;
-            defaultDurebility = durability;
-            defaultKnowledge = knowledge;
+            hp = status.HitPoint;
+            attack = status.Attack;
+            defense = status.Defense;
+            charm = status.Charm;
+            dignity = status.Dignity;
+            muscularStrength = status.MuscularStrength;
+            sense = status.Sense;
+            durability = status.Durability;
+            knowledge = status.Knowledge;
 
             addCharm = 0;
             addDignity = 0;
@@ -402,10 +393,14 @@ namespace DemonicCity.StrengthenScene
             statusPoint = 99;//debug
 
             UpdateText();
+
+            confirmMessageWindow.SetActive(false);
+            activePopUpWindow = false;
+            changedStatus = false;
         }
 
         /// <summary>変更したステータスを確定する</summary>
-        public void ConfirmStatus()
+        private void ConfirmStatus()
         {
             hp = updatedHitPoint;
             attack = updatedAttack;
@@ -427,20 +422,22 @@ namespace DemonicCity.StrengthenScene
 
             magia.Sync();
             UpdateText();
+
+            confirmMessageWindow.SetActive(false);
+            activePopUpWindow = false;
+            changedStatus = false;
         }
 
         /// <summary>割り振りポイント-1、固有ステータスポイント+1</summary>
         /// <param name="uniqueStatus">固有ステータス</param>
-        public int AddStatusPoint(int uniqueStatus)
+        private int AddStatusPoint(int uniqueStatus)
         {
             if (statusPoint > 0)
             {
                 statusPoint -= 1;
                 statusPointText.text = statusPoint.ToString();
                 uniqueStatus = 1;
-
-                allocationPoint += 1;
-            }
+}
             else
             {
                 uniqueStatus = 0;
@@ -450,7 +447,7 @@ namespace DemonicCity.StrengthenScene
         }
 
         /// <summary>テキストを更新</summary>
-        public void UpdateText()
+        private void UpdateText()
         {
             currentBasicStatusTexts[0].text = hp.ToString();
             currentBasicStatusTexts[1].text = attack.ToString();
