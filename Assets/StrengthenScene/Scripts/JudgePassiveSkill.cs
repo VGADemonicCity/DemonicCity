5ce0ac66220994424b538ffdd4db51c3ca4f1ccf
RyoOkazaki <ryolavieg@hotmail.co.jp>
2019-03-23 14:19:23 +0900

    強化画面　習得済みスキルの表示機能実装

---
+++ Assets/StrengthenScene/Scripts/JudgePassiveSkill.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DemonicCity.StrengthenScene
+{
+    public class JudgePassiveSkill : JudgementMasteringSkill
+    {
+
+        public override void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void JudgementMastering(Magia.PassiveSkill passiveSkill)
+        {
+            //if ((magia.MyPassiveSkill & passiveSkill) == passiveSkill)
+            //{
+            //    isMastering = true;
+            //    gameObject.SetActive(true);
+            //    Debug.Log(passiveSkill + "は習得済み");
+            //}
+            //else
+            //{
+            //    isMastering = false;
+            //    gameObject.SetActive(false);
+            //    Debug.Log(passiveSkill + "は未習得");
+            //}
+
+            if(magia.MyPassiveSkill >= passiveSkill)
+            {
+                isMastering = true;
+                gameObject.SetActive(true);
+                Debug.Log(passiveSkill + "は習得済み");
+            }
+            else
+            {
+                isMastering = false;
+                gameObject.SetActive(false);
+                Debug.Log(passiveSkill + "は未習得");
+            }
+        }
+    }
+}
