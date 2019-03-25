50287e00d3620d2f32f8f42a701ebcf9d386e81f
Uchida <laver9692@gmail.com>
2019-03-25 21:33:18 +0900

    Revert "Progress改変"
    This reverts commit 371dfc46735c25ab75b997a8880e017dffff7320.

--- Assets/StoryScene/Script/Progress.cs
+++ Assets/StoryScene/Script/Progress.cs
@@ -9,17 +9,8 @@ namespace DemonicCity
     /// <summary>
     ///ストーリーの進行度、進捗
     ///</summary>
-    public class Progress : MonoSingleton<Progress>
+    public class Progress : SavableSingletonBase<Progress>
     {
-
-        SaveData saveData;
-
-        public override void OnInitialize()
-        {
-            base.OnInitialize();
-            saveData = SaveData.Instance;
-            m_instance = Instantiate(saveData.progress);
-        }
 
         /// <summary>ストーリーの進行度</summary>
         [Flags]
@@ -52,7 +43,7 @@ namespace DemonicCity
             /// <summary>12章</summary>
             Ixmagina = 4096,
             All = 8191,
-            Test = 8192,
+            Test=8192,
         }
         /// <summary>1クエスト内での進行度</summary>
 
@@ -102,7 +93,7 @@ namespace DemonicCity
 
         public StoryProgress NextStory(StoryProgress nowStory)
         {
-            int tmpStory = 0;
+            int tmpStory=0;
             foreach (StoryProgress story in Enum.GetValues(typeof(StoryProgress)))
             {
                 if ((nowStory & story) == story)
@@ -112,15 +103,6 @@ namespace DemonicCity
             }
             tmpStory = tmpStory << 1;
             return (StoryProgress)tmpStory;
-        }
-
-        void Save()
-        {
-            if (saveData == null)
-            {
-                saveData = SaveData.Instance;
-            }
-            saveData.progress = this;
         }
     }
 
