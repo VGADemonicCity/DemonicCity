5974afe21f1fcb4b4ce4aabe2d970d13e8465f2d
Uchida <laver9692@gmail.com>
2019-03-25 21:18:14 +0900

    ChapterにScriptableObjectを継承させた。 それに伴う修正

--- Assets/Scripts/Chapter.cs
+++ Assets/Scripts/Chapter.cs
@@ -8,7 +8,8 @@ namespace DemonicCity
     /// 各章の情報が格納されるクラス
     /// </summary>
     [System.Serializable]
-    public class Chapter
+    [CreateAssetMenu(fileName = "ChapterData", menuName = "CreChapter")]
+    public class Chapter : ScriptableObject
     {
         /// <summary>その章で使うEnemiesId</summary>
         public List<EnemiesFactory.EnemiesId> enemiesIds = new List<EnemiesFactory.EnemiesId>();
@@ -20,30 +21,52 @@ namespace DemonicCity
         public bool isBattle;
         /// <summary>その章に会話があるか</summary>
         public bool isStory;
-        /// <summary>その章のソースフォルダのパス</summary>
-        public string sourceFolderPath;
+        ///// <summary>その章のソースフォルダのパス</summary>
+        //public string sourceFolderPath;
+        /// <summary>使用する背景</summary>
+        public List<Sprite> BackGround = new List<Sprite>();
+        /// <summary>戦闘で使用するステージ</summary>
+        public List<Sprite> BattleStage = new List<Sprite>();
+        /// <summary>バトル前シナリオ</summary>
+        [SerializeField] StoryScene.Scenario prologue;
+        /// <summary>バトル後シナリオ</summary>
+        [SerializeField] StoryScene.Scenario epilogue;
+        /// <summary>テスト用シナリオ</summary>
+        [SerializeField] StoryScene.Scenario test;
 
+        public Dictionary<Progress.QuestProgress, StoryScene.Scenario> scenario
+        {
+            get
+            {
+                return new Dictionary<Progress.QuestProgress, StoryScene.Scenario>()
+                {
+                    { Progress.QuestProgress.Prologue,prologue },
+                    { Progress.QuestProgress.Epilogue,epilogue},
+                    { Progress.QuestProgress.Test,test}
+                };
+            }
+        }
 
-        /// <summary>ストーリーの進行度とタイトル、敵のIDを3Wave分登録する</summary>
-        public Chapter(string title, Progress.StoryProgress progress, EnemiesFactory.EnemiesId enemy1, EnemiesFactory.EnemiesId enemy2, EnemiesFactory.EnemiesId enemy3, string folderPath,bool story=true)
-        {
-            chapterTitle = title;
-            storyProgress = progress;
-            sourceFolderPath = folderPath;
-            enemiesIds.Add(enemy1);
-            enemiesIds.Add(enemy2);
-            enemiesIds.Add(enemy3);
-            isBattle = true;
-            isStory = story;
-        }
-        /// <summary>ストーリーの進行度とタイトルを登録する</summary>
-        public Chapter(string title, Progress.StoryProgress progress,string folderPath,bool story=true)
-        {
-            chapterTitle = title;
-            storyProgress = progress;
-            sourceFolderPath = folderPath;
-            isBattle = false;
-            isStory = story;
-        }
+        ///// <summary>ストーリーの進行度とタイトル、敵のIDを3Wave分登録する</summary>
+        //public Chapter(string title, Progress.StoryProgress progress, EnemiesFactory.EnemiesId enemy1, EnemiesFactory.EnemiesId enemy2, EnemiesFactory.EnemiesId enemy3, string folderPath, bool story = true)
+        //{
+        //    chapterTitle = title;
+        //    storyProgress = progress;
+        //    sourceFolderPath = folderPath;
+        //    enemiesIds.Add(enemy1);
+        //    enemiesIds.Add(enemy2);
+        //    enemiesIds.Add(enemy3);
+        //    isBattle = true;
+        //    isStory = story;
+        //}
+        ///// <summary>ストーリーの進行度とタイトルを登録する</summary>
+        //public Chapter(string title, Progress.StoryProgress progress, string folderPath, bool story = true)
+        //{
+        //    chapterTitle = title;
+        //    storyProgress = progress;
+        //    sourceFolderPath = folderPath;
+        //    isBattle = false;
+        //    isStory = story;
+        //}
     }
 }
--- Assets/Scripts/ChapterManager.cs
+++ Assets/Scripts/ChapterManager.cs
@@ -24,24 +24,38 @@ namespace DemonicCity
         /// <summary>ストーリー進行度のインスタンスの参照</summary>
         Progress progress;
 
+
+        List<Chapter> chapters = new List<Chapter>();
+
+
         /// <summary>進行度と敵のIDを登録するList</summary>
-        public List<Chapter> chapters = new List<Chapter>()
+        public List<Chapter> Chapters
         {
-            new Chapter("プロローグ",Progress.StoryProgress.Prologue,sourceFolderPath+"1/"),
-            new Chapter("はじまりと邂逅",Progress.StoryProgress.Phoenix, EnemiesFactory.EnemiesId.Phoenix, EnemiesFactory.EnemiesId.Phoenix, EnemiesFactory.EnemiesId.Phoenix,sourceFolderPath+"2/"),
-            new Chapter("道なき道へ",Progress.StoryProgress.Nafla, EnemiesFactory.EnemiesId.SingleHorn, EnemiesFactory.EnemiesId.DoubleHorns, EnemiesFactory.EnemiesId.Nafla,sourceFolderPath+"3/"),
-            new Chapter("迫る鎮圧部隊",Progress.StoryProgress.ZAKO1,sourceFolderPath+"4/",false),
-            new Chapter("消えぬ炎",Progress.StoryProgress.Amon,sourceFolderPath+"5/"),
-            new Chapter("怒り狂う軍隊",Progress.StoryProgress.ZAKO2,sourceFolderPath+"6/",false),
-            new Chapter("理の誘い",Progress.StoryProgress.Ashmedy,sourceFolderPath+"7/"),
-            new Chapter("立ちふさがる防衛隊",Progress.StoryProgress.ZAKO3,sourceFolderPath+"8/",false),
-            new Chapter("恐怖への扉",Progress.StoryProgress.Faulus,sourceFolderPath+"9/"),
-            new Chapter("魔王親衛隊",Progress.StoryProgress.ZAKO4,sourceFolderPath+"10/",false),
-            new Chapter("決戦の覚悟",Progress.StoryProgress.Barl,sourceFolderPath+"11/"),
-            new Chapter("偽りの仮面",Progress.StoryProgress.InvigoratedPhoenix,sourceFolderPath+"12/"),
-            new Chapter("終末の王",Progress.StoryProgress.Ixmagina,sourceFolderPath+"13/"),
-            new Chapter("Test",Progress.StoryProgress.Test,sourceFolderPath+""),
-        };
+            get
+            {
+                if (chapters.Count == 0)
+                {
+                    chapters = Resources.LoadAll<Chapter>(sourceFolderPath).ToList();
+                }
+
+                return chapters;
+
+            }
+            //new Chapter("プロローグ",Progress.StoryProgress.Prologue,sourceFolderPath+"1/"),
+            //new Chapter("はじまりと邂逅",Progress.StoryProgress.Phoenix, EnemiesFactory.EnemiesId.Phoenix, EnemiesFactory.EnemiesId.Phoenix, EnemiesFactory.EnemiesId.Phoenix,sourceFolderPath+"2/"),
+            //new Chapter("道なき道へ",Progress.StoryProgress.Nafla, EnemiesFactory.EnemiesId.SingleHorn, EnemiesFactory.EnemiesId.DoubleHorns, EnemiesFactory.EnemiesId.Nafla,sourceFolderPath+"3/"),
+            //new Chapter("迫る鎮圧部隊",Progress.StoryProgress.ZAKO1,sourceFolderPath+"4/",false),
+            //new Chapter("消えぬ炎",Progress.StoryProgress.Amon,sourceFolderPath+"5/"),
+            //new Chapter("怒り狂う軍隊",Progress.StoryProgress.ZAKO2,sourceFolderPath+"6/",false),
+            //new Chapter("理の誘い",Progress.StoryProgress.Ashmedy,sourceFolderPath+"7/"),
+            //new Chapter("立ちふさがる防衛隊",Progress.StoryProgress.ZAKO3,sourceFolderPath+"8/",false),
+            //new Chapter("恐怖への扉",Progress.StoryProgress.Faulus,sourceFolderPath+"9/"),
+            //new Chapter("魔王親衛隊",Progress.StoryProgress.ZAKO4,sourceFolderPath+"10/",false),
+            //new Chapter("決戦の覚悟",Progress.StoryProgress.Barl,sourceFolderPath+"11/"),
+            //new Chapter("偽りの仮面",Progress.StoryProgress.InvigoratedPhoenix,sourceFolderPath+"12/"),
+            //new Chapter("終末の王",Progress.StoryProgress.Ixmagina,sourceFolderPath+"13/"),
+            //new Chapter("Test",Progress.StoryProgress.Test,sourceFolderPath+""),
+        }
 
 
 
@@ -52,7 +66,7 @@ namespace DemonicCity
         /// <returns>敵のIDのList</returns>
         public List<EnemiesFactory.EnemiesId> GetEnemies(Progress.StoryProgress progress)
         {
-            Chapter chapter = chapters.Find(item => item.storyProgress == progress);
+            Chapter chapter = Chapters.Find(item => item.storyProgress == progress);
             return chapter.enemiesIds;
         }
         /// <summary>
@@ -61,7 +75,7 @@ namespace DemonicCity
         public List<EnemiesFactory.EnemiesId> GetEnemies()
         {
             CheckProgress();
-            Chapter chapter = chapters.Find(item => item.storyProgress == progress.ThisStoryProgress);
+            Chapter chapter = Chapters.Find(item => item.storyProgress == progress.ThisStoryProgress);
             return chapter.enemiesIds;
         }
 
@@ -73,7 +87,7 @@ namespace DemonicCity
         /// <returns></returns>
         public string GetTitle(Progress.StoryProgress progress)
         {
-            Chapter chapter = chapters.Find(item => item.storyProgress == progress);
+            Chapter chapter = Chapters.Find(item => item.storyProgress == progress);
             return chapter.chapterTitle;
         }
         /// <summary>
@@ -82,7 +96,7 @@ namespace DemonicCity
         public string GetTitle()
         {
             CheckProgress();
-            Chapter chapter = chapters.Find(item => item.storyProgress == progress.ThisStoryProgress);
+            Chapter chapter = Chapters.Find(item => item.storyProgress == progress.ThisStoryProgress);
             return chapter.chapterTitle;
         }
         /// <summary>
@@ -91,7 +105,7 @@ namespace DemonicCity
         /// <returns></returns>
         public Chapter GetChapter(Progress.StoryProgress progress)
         {
-            return chapters.Find(item => item.storyProgress == progress);
+            return Chapters.Find(item => item.storyProgress == progress);
         }
         /// <summary>
         /// 現在進行している章のChapterを取得する
@@ -100,7 +114,7 @@ namespace DemonicCity
         public Chapter GetChapter()
         {
             CheckProgress();
-            return chapters.Find(item => item.storyProgress == progress.ThisStoryProgress);
+            return Chapters.Find(item => item.storyProgress == progress.ThisStoryProgress);
         }
 
 
@@ -116,6 +130,6 @@ namespace DemonicCity
         }
 
 
-        const string sourceFolderPath = "D:/SourceTree/DemonicCity/Assets/StoryScene/Sources/";
+        const string sourceFolderPath = "ChapterData/";
     }
 }
