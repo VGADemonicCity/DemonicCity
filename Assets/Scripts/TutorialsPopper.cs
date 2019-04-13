6d5c153fa403c81ce83c784dad9fa5e017788116
Zukky_13 <www.kase@gmail.com>
2019-04-14 07:41:55 +0900

    チュートリアル作成作業開始前バックアップ

--- Assets/Scripts/TutorialsPopper.cs
+++ Assets/Scripts/TutorialsPopper.cs
@@ -1,17 +1,40 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 namespace DemonicCity
 {
     public class TutorialsPopper : MonoBehaviour
     {
+        /// <summary>表示させるチュートリアルの素材群</summary>
         [SerializeField] List<TutorialItems> items;
+        /// <summary>popup system</summary>
         [SerializeField] PopupSystem popupSystem;
 
+        /// <summary>現在対象となっている素材</summary>
+        TutorialItems currentItem;
+        
         void Popup()
         {
 
+        }
+
+
+
+        void ToNextTexture()
+        {
+
+        }
+
+        bool HasNextTexture()
+        {
+
+
+
+            return true;
         }
     }
 }
