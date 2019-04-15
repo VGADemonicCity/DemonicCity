7213e4aaed144ce91355f26dd2e6bd23e38d05b1
Zukky_13 <www.kase@gmail.com>
2019-04-15 19:11:29 +0900

    pc移動

--- Assets/BattleScene/Scripts/Tutorial/BattleSceneTutorialsPopper.cs
+++ Assets/BattleScene/Scripts/Tutorial/BattleSceneTutorialsPopper.cs
@@ -40,6 +40,8 @@ namespace DemonicCity.BattleScene
         [SerializeField] TutorialItems tutorialObject;
         /// <summary>閉じるボタン</summary>
         [SerializeField] Button okButton;
+        /// <summary>itween animationに使う時間</summary>
+        [SerializeField] float fadingTime = .5f;
 
         /// <summary>popup system</summary>
         PopupSystem popupSystem;
@@ -51,6 +53,12 @@ namespace DemonicCity.BattleScene
         Button popupedOkButton;
         /// <summary>対象の</summary>
         Image targetImage;
+        /// <summary>次へボタン</summary>
+        Button popupedToNextButton;
+        /// <summary>チュートリアル画像を生成するオブジェクトの親</summary>
+        GameObject tutorialImagesParent;
+
+        AudioSource audioSource;
 
         const int width = 1080;
         const int height = 1920;
@@ -84,14 +92,83 @@ namespace DemonicCity.BattleScene
             }
             else
             {
-            currentItem = items.First();
-
+                currentItem = items.First();
+                var go = new GameObject();
+                go.transform.parent = popupSystem.popupedObject.transform.GetChild(0);
             }
 
-            targetImage.sprite = currentItem.Sprite;
             if (currentItem.useVoice)
             {
                 //SoundManager.Instance.PlayWithFade(SoundManager.SoundTag.SE, clip);
+            }
+        }
+
+        /// <summary>
+        /// 次へボタンのイベントハンドラ
+        /// </summary>
+        void OnPushNextButton()
+        {
+            ChangeItem(Index.Next);
+            FadingImage(Index.Next);
+            OnChangeItem();
+        }
+
+        /// <summary>
+        /// Itemを変更した時
+        /// </summary>
+        void OnChangeItem()
+        {
+
+
+
+            var soundManaegr = SoundManager.Instance;
+
+            // 前の画面で再生されていた音声を停止して次の音声が存在する場合音声を再生させる
+            soundManaegr.PlayWithFade(SoundManager.SoundTag.Voice, null);
+            if (currentItem.useVoice)
+            {
+                soundManaegr.PlayWithFade(SoundManager.SoundTag.Voice, currentItem.VoiceClip);
+            }
+        }
+
+        /// <summary>
+        /// fading image
+        /// </summary>
+        void FadingImage(Index index)
+        {
+
+            switch (index)
+            {
+                case Index.Next:
+                    iTween.MoveBy(tutorialImagesParent, iTween.Hash("amount", new Vector3(-width, 0), "time", fadingTime));
+                    break;
+                case Index.Previous:
+                    iTween.MoveBy(tutorialImagesParent, iTween.Hash("amount", new Vector3(width, 0), "time", fadingTime));
+                    break;
+                case Index.Last:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Itemを適切な要素へ変更する
+        /// </summary>
+        /// <param name="direction"></param>
+        void ChangeItem(Index direction)
+        {
+            var index = tutorialObject.Items.IndexOf(currentItem);
+            switch (direction)
+            {
+                case Index.Next:
+                    currentItem = tutorialObject.Items[++index];
+                    break;
+                case Index.Previous:
+                    currentItem = tutorialObject.Items[--index];
+                    break;
+                default:
+                    throw new System.ArgumentException("Directionが適切に指定されていません.");
             }
         }
 
@@ -101,8 +178,52 @@ namespace DemonicCity.BattleScene
             battleManager.SetStateMachine(battleManager.m_StateMachine.m_PreviousState);
         }
 
-
+        /// <summary>
+        /// ボタンが表示可能かどうかチェックし,trueならコンポーネントを有効に,falseなら無効にする
+        /// /// </summary>
+        /// <param name="button"></param>
+        /// <param name="direction"></param>
+        void CheckButtonVibible(Button button, Index direction)
+        {
+            switch (direction)
+            {
+                case Index.Next:
+                    if (NextItem != null)
+                    {
+                        button.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        button.gameObject.SetActive(false);
+                    }
+                    break;
+                case Index.Previous:
+                    if (PreviousItem != null)
+                    {
+                        button.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        button.gameObject.SetActive(false);
+                    }
+                    break;
+                case Index.Last:
+                    var index = tutorialObject.Items.IndexOf(currentItem);
+                    if (index == tutorialObject.Items.Count - 1)
+                    {
+                        button.gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        button.gameObject.SetActive(false);
+                    }
+                    break;
+                default:
+                    throw new System.ArgumentException("Directionが適切に指定されていません.");
+            }
+        }
     }
+
     public enum Subject
     {
         One,
