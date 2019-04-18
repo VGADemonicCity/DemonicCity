7c2447ec16c8540daef37eafe24e846a5c7e1bba
RyoOkazaki <ryolavieg@hotmail.co.jp>
2019-04-19 00:16:29 +0900

    リザルト画面　遷移先ポップアップ　不具合修正

--- Assets/StrengthenScene/Scripts/JudgePassiveSkill.cs
+++ Assets/StrengthenScene/Scripts/JudgePassiveSkill.cs
@@ -20,19 +20,6 @@ namespace DemonicCity.StrengthenScene
 
         public override void JudgementMastering(Magia.PassiveSkill passiveSkill)
         {
-            //if (magia.MyPassiveSkill >= passiveSkill)
-            //{
-            //    isMastering = true;
-            //    gameObject.SetActive(true);
-            //    Debug.Log(passiveSkill + "は習得済み");
-            //}
-            //else
-            //{
-            //    isMastering = false;
-            //    gameObject.SetActive(false);
-            //    Debug.Log(passiveSkill + "は未習得");
-            //}
-
             if ((magia.MyPassiveSkill & passiveSkill) == passiveSkill)
             {
                 gameObject.SetActive(true);
