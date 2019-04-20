8a79cfe872ad7800a01c082a16ad209d46e10dba
Zukky_13 <www.kase@gmail.com>
2019-04-20 20:25:47 +0900

    解像度固定処理作成中

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,10 @@ namespace DemonicCity
     /// </summary>
     public class GameManager : MonoSingleton<GameManager>
     {
+
+        const float widthRatio = 9f;
+        const float heightRatio = 16f;
+
         /// <summary></summary>
         SceneFader m_sceneFader;
 
@@ -21,7 +25,26 @@ namespace DemonicCity
             {
                 m_sceneFader.FadeIn(); // SceneLoad時の画面演出
                 Time.timeScale = 1f;
+                // 解像度を調整する
+                AdjustmentRatio();
             });
+        }
+
+        void AdjustmentRatio()
+        {
+            var camera = GameObject.FindGameObjectWithTag("Camera").GetComponent<Camera>();
+
+            // 理想の画面の比率
+            float targetRatio = widthRatio / heightRatio;
+            // 現在の画面の比率
+            float currentRatio = Screen.width * 1f / Screen.height;
+            // 理想と現在の比率
+            float ratio = targetRatio / currentRatio;
+
+            //カメラの描画開始位置をX座標にどのくらいずらすか
+            float rectX = (1.0f - ratio) / 2f;
+            //カメラの描画開始位置と表示領域の設定
+            camera.rect = new Rect(rectX, 0f, ratio, 1f);
         }
 
 
