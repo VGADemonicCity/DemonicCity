004ef4ad97f69d0054af128eba9c589e1709d0c0
Uchida <laver9692@gmail.com>
2019-04-26 12:46:06 +0900

    Titleに戻るボタン追加

---
+++ Assets/HomeScene/Scripts/Title/ToTitle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DemonicCity
+{
+    public class ToTitle : MonoBehaviour
+    {
+        [SerializeField] UnityEngine.UI.Button btn;
+        public void ToTitleScene()
+        {
+            SceneFader.Instance.FadeOut(SceneFader.SceneTitle.Title);
+            btn.interactable = false;
+        }
+    }
+}
