0d53128bffe07546d8ec43e7b26f2772aa64df1a
Zukky_13 <www.kase@gmail.com>
2019-04-27 23:38:41 +0900

    複数解像度対応中

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,89 +26,26 @@ namespace DemonicCity
                 m_sceneFader.FadeIn(); // SceneLoad時の画面演出
                 Time.timeScale = 1f;
                 // 解像度を調整する
-                //AdjustmentRatio();
+                AdjustmentRatio();
             });
         }
 
-        //void AdjustmentRatio()
-        //{
-        //    var camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-
-        //    // 理想の画面の比率
-        //    float targetRatio = widthRatio / heightRatio;
-        //    // 現在の画面の比率
-        //    float currentRatio = Screen.width * 1f / Screen.height;
-        //    // 理想と現在の比率
-        //    float ratio = targetRatio / currentRatio;
-
-        //    //カメラの描画開始位置をX座標にどのくらいずらすか
-        //    float rectX = (1.0f - ratio) / 2f;
-        //    //カメラの描画開始位置と表示領域の設定
-        //    camera.rect = new Rect(rectX, 0f, ratio, 1f);
-        //}
-
-        //// Use this for initialization
-        //void Start()
-        //{
-
-        //    // set the desired aspect ratio (the values in this example are
-        //    // hard-coded for 16:9, but you could make them into public
-        //    // variables instead so you can set them at design time)
-
-        //    float targetaspect = 9.0f / 16.0f;
-
-        //    // determine the game window's current aspect ratio
-
-        //    float windowaspect = (float)Screen.width / (float)Screen.height;
-
-        //    // current viewport height should be scaled by this amount
-
-        //    float scaleheight = windowaspect / targetaspect;
-
-        //    // obtain camera component so we can modify its viewport
-
-        //    var camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        //    //Camera camera = GetComponent<Camera>();
-
-        //    // if scaled height is less than current height, add letterbox
-
-        //    if (scaleheight < 1.0f)
-        //    {
-        //        Rect rect = camera.rect;
-
-        //        rect.width = 1.0f;
-
-        //        rect.height = scaleheight;
-        //        rect.x = 0;
-        //        rect.y = (1.0f - scaleheight) / 2.0f;
-
-        //        camera.rect = rect;
-
-        //    }
-        //    else // add pillarbox
-        //    {
-        //        float scalewidth = 1.0f / scaleheight;
-
-        //        Rect rect = camera.rect;
-
-        //        rect.width = scalewidth;
-
-        //        rect.height = 1.0f;
-        //        rect.x = (1.0f - scalewidth) / 2.0f;
-        //        rect.y = 0;
-
-        //        camera.rect = rect;
-
-        //    }
-        //}
-
-        // Update is called once per frame
-        void Update()
+        void AdjustmentRatio()
         {
+            var camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
+            // 理想の画面の比率
+            float targetRatio = widthRatio / heightRatio;
+            // 現在の画面の比率
+            float currentRatio = Screen.width / Screen.height;
+            // 理想と現在の比率
+            float ratio = targetRatio / currentRatio;
+
+            //カメラの描画開始位置をX座標にどのくらいずらすか
+            float rectX = (1.0f - ratio) / 2f;
+            //カメラの描画開始位置と表示領域の設定
+            camera.rect = new Rect(rectX, 0f, ratio, 1f);
         }
-
-
 
         /// <summary>
         /// 作ったゲームオブジェクトをシーン遷移しても壊されない様にする
