aee41fb3e2ecf6b153925b92731e928aa3b0ca6d
Zukky_13 <www.kase@gmail.com>
2019-05-04 14:28:14 +0900

    デバッグ機能非表示後コミット

--- Assets/Scenes/TestSecenes/touch/touchTest.cs
+++ Assets/Scenes/TestSecenes/touch/touchTest.cs
@@ -7,7 +7,7 @@ namespace DemonicCity
     public class touchTest : MonoBehaviour
     {
         TouchGestureDetector touchGestureDetector;
-
+        [SerializeField] GameObject obj;
         private void Awake()
         {
             touchGestureDetector = TouchGestureDetector.Instance;
@@ -22,6 +22,7 @@ namespace DemonicCity
 
                     GameObject hitResultGameObject;
                     var HitResult = touchInfo.HitDetection(out hitResultGameObject);
+                        Destroy(Instantiate(obj, touchInfo.LastPosition, Quaternion.identity), 3f);
                     if (HitResult)
                     {
                         Debug.Log(hitResultGameObject.name);
