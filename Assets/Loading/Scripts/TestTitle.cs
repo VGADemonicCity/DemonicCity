807ded8b619a063d9b63df9696c291b1bbb61292
Uchida <laver9692@gmail.com>
2019-07-18 15:33:21 +0900

    InLoadingTest

---
+++ Assets/Loading/Scripts/TestTitle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTitle : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ToTitle()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(DemonicCity.SceneFader.SceneTitle.Title.ToString());
+    }
+}
